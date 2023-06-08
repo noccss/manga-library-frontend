809c82c68d159cc43bce3b3f5697995c89762d92
Noccss <nicolas.olicarvalho@gmail.com>
2023-06-07 22:54:01 -0300

    Add simple header struct

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "./style.css";
+
+export default function Header() {
+  return (
+    <header className="header-main">
+      <a href="#" className="header-icon">Manga Library</a>
+      <nav className="header-items">
+        <a href="#" className="header-buttons">Entrar</a>
+      </nav>
+    </header>
+  );
+}
